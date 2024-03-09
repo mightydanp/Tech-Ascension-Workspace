7027b423edd5b1502c7105c3daf06cb7f694f04e
mightydanp <chrisdunbar12@gmail.com>
2024-03-08 22:25:05 -0600

    working AT workspace

---
+++ switch1192.sh
@@ -0,0 +1,26 @@
+pushd InControl
+git checkout 1.19
+popd
+
+pushd FxControl
+git checkout 1.19
+popd
+
+pushd LostCities
+git checkout 1.19
+popd
+
+pushd LostSouls
+git checkout 1.19
+popd
+
+pushd McJtyLib
+git checkout 1.19
+popd
+
+pushd RFToolsDimensions
+git checkout 1.19
+popd
+
+git checkout 1.19_tech
+
